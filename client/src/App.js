96ed8479a0b458e635fac84b1a7c322a12268a4d
= <=>
2018-04-21 19:21:22 -0400

    add Sass to project and create Title component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/pokeball.png';
 import './App.css';
+
+// Components
+import Title from './Components/Title/Title';
 
 class App extends Component {
   render() {
@@ -8,11 +11,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Title />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ client/src/Components/Title/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import '../../Sass/Components/Title.css';
+
+export default class Title extends Component {
+  render() {
+    return (
+      <h1 className='title'>Pokemon Guess</h1>
+    );
+  }
+}
